16dca2f688dc1308fef113430f5c30916ab4bef9
strubix <lucas.strubi@gmail.com>
2016-10-28 23:07:06 +0200

    film repository

--- app/src/MainBundle/Controller/DefaultController.php
+++ app/src/MainBundle/Controller/DefaultController.php
@@ -17,6 +17,22 @@ class DefaultController extends Controller
         $pagination = $paginator->paginate($movies, $request->query->getInt('page', 1), 25);
 
         $form = $this->createForm(FilmType::class);
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $data = $request->get('mainbundle_film');
+
+
+            $em = $this->getDoctrine()->getManager();
+            $movies = $em->getRepository('MainBundle:Film')
+                ->findFilmByFilter($data);
+
+            $pagination = $paginator->paginate($movies, $request->query->getInt('page', 1), 25);
+
+            return $this->render('MainBundle:Default:index.html.twig', array(
+                'movies' => $pagination,
+                'form' => $form->createView()
+            ));
+        }
 
         return $this->render('MainBundle:Default:index.html.twig', array(
             'movies' => $pagination,
